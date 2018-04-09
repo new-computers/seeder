2cace191c20bf2aec947e72b1befc6da33d3ac5b
Hunor Karamán <hunix94@gmail.com>
2018-04-09 21:57:17 +0300

    autostart

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ npm i -g hypercored add-to-systemd lil-pids
 npm install
 npm run build
 sudo add-to-systemd dat-lil-pids $(which lil-pids) ~/seeder/services ~/seeder/pids
+sudo systemctl start dat-lil-pids
