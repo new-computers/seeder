a1343369b4fe46961ddb448aaa2c22766a22755b
Hunor Karamán <hunix94@gmail.com>
2018-04-11 10:30:42 +0300

    new structure + store

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const koa = require('koa')
 const serve = require('koa-static')
 const bodyParser = require('koa-bodyparser')
-const Router = require('koa-router')
+const Dat = require('dat-node')
 
-const fs = require('fs')
+const Store = require('./lib/store')
+const router = require('./lib/router')
+const Archiver = require('./lib/archiver')
 
 const app = new koa()
 
@@ -15,60 +17,17 @@ if (process.argv.length > 2 && process.argv[2] == 'dev') {
 	root = '.'
 }
 
+const store = new Store(root + '/data.json')
+const archiver = new Archiver(root, store)
+const r = router(store, archiver)
+
+store.read()
+archiver.update()
+
 app.use(serve(root + '/bundles'))
 app.use(bodyParser())
-
-const router = new Router()
-
-router.get('/feeds', async (ctx, next) => {
-	var feeds = read()
-
-	feeds = feeds.split('\n').filter((f) => f.length > 0)
-
-	ctx.response.set('Content-Type', 'application/json')
-	ctx.body = {feeds: feeds}
-})
-
-router.post('/feeds', async (ctx, next) => {
-	var feeds = read()
-
-	feeds += ctx.request.body.url + '\n'
-
-	fs.writeFileSync(root + '/feeds', feeds)
-
-	ctx.response.set('Content-Type', 'application/json')
-	ctx.body = {success: true}
-})
-
-router.post('/remove-feed', async (ctx, next) => {
-	var feeds = read()
-
-	feeds = feeds.split('\n').filter(f => f.length > 0)
-	feeds.splice(feeds.indexOf(ctx.request.body.url), 1)
-
-	var r = ''
-	for (var i = 0; i < feeds.length; i++) {
-		r += feeds[i] + '\n'
-	}
-
-	fs.writeFileSync(root + '/feeds', r)
-
-	ctx.response.set('Content-Type', 'application/json')
-	ctx.body = {success: true}
-})
-
-app.use(router.routes())
-	.use(router.allowedMethods())
-
-function read() {
-	var feeds = ''
-
-	try {
-		feeds = fs.readFileSync(root + '/feeds', 'utf8')
-	} catch (e) {}
-
-	return feeds
-}
+app.use(r.routes())
+	.use(r.allowedMethods())
 
 console.log('Server listening on port ' + port + '...')
 app.listen(port)
