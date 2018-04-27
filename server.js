7de0b9e86516e78befa5280118402cc862c3b5e4
Hunor Karamán <hunix94@gmail.com>
2018-04-27 22:28:04 +0300

    omg it works, kinda

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const koa = require('koa')
 const http = require('http')
 const serve = require('koa-static')
-const vhost = require('koa-vhost')
+//const vhost = require('koa-vhost')
+const Switch = require('koa-switch-vhost');
 const Router = require('koa-router')
 const bodyParser = require('koa-bodyparser')
 
@@ -28,21 +29,50 @@ const r = router(store, archiver)
 store.read()
 archiver.all()
 
-app.use(serve(root + '/bundles'))
+const vhost = new Switch();
+
+var sites = store.get('feeds')
+for (var i = 0; i < sites.length; i++) {
+	if (sites[i].http) {
+		vhost.use({
+			host: sites[i].http,
+			app: create_site_app(sites[i])
+		})
+	}
+}
+
 app.use(bodyParser())
-app.use(r.routes())
-	.use(r.allowedMethods())
+app.use(r.routes()).use(r.allowedMethods())
+app.use(vhost.switch())
+app.use(serve(root + '/bundles'))
 
 var server
 if (!dev) {
-	// lets encrypt
+	// TODO: lets encrypt
 } else {
-	server = http.createServer(app)
+	server = http.createServer(app.callback())
 	server.listen(port)
 }
 server.once('listening', function() {
 	console.log('The frontend is served on port ' + port + '.\n')
 })
+
+function create_site_app(site) {
+	var sapp = new koa()
+
+	var router = new Router()
+	router.get('/.well-known/dat', function(ctx) {
+		ctx.body = site.url
+	})
+
+	var dirname = site.url.replace('dat://', '').replace(/\//g, '')
+
+	sapp.use(serve(root + '/archives/' + dirname, {extensions: ['html']}))
+	sapp.use(router.routes()).use(router.allowedMethods())
+
+	return sapp
+}
+
 
 // Timeout for seeded sites
 check_timeouts()
